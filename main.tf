c70f5685a41aabfd2629fdc897056633138e8286
Kostis Kapelonis <kostis@codefresh.io>
2019-05-27 13:48:45 +0300

    First commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "firstkubernetes-176201"
+  region  = "europe-west2"
+  zone    = "europe-west2-b"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
+
